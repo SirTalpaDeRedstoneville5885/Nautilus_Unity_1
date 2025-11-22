657fc0a7a93d099678c43f7cf616388f1ba28d33
SirTalpaDeRedstoneville5885 <carnotauro9351@gmail.com>
2025-11-22 18:37:38 +0100

    secondo commit

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] GameObject pos1,pos2, nextPos;
+    [SerializeField] GameObject pos1, pos2, nextPos;
     [SerializeField] float speed;
     [SerializeField] GameObject enemyBody;
     void Update()
     {
-        if (Vector2.Distance(enemyBody.transform.position, nextPos.transform.position) >= 0) {
-            enemyBody.transform.position = Vector2.MoveTowards(enemyBody.transform.position, nextPos.transform.position, speed*Time.deltaTime);
-        };
-        if (Vector2.Distance(enemyBody.transform.position, pos2.transform.position)==0){
+        if (Vector2.Distance(enemyBody.transform.position, nextPos.transform.position) >= 0)
+        {
+            enemyBody.transform.position = Vector2.MoveTowards(enemyBody.transform.position, nextPos.transform.position, speed * Time.deltaTime);
+        }
+        ;
+        if (Vector2.Distance(enemyBody.transform.position, pos2.transform.position) == 0)
+        {
             nextPos = pos1;
-        };
-        if (Vector2.Distance(enemyBody.transform.position, pos1.transform.position)==0){
+        }
+        ;
+        if (Vector2.Distance(enemyBody.transform.position, pos1.transform.position) == 0)
+        {
             nextPos = pos2;
-        };
+        }
+        ;
     }
 }
--- Assets/Scripts/kill.cs
+++ Assets/Scripts/kill.cs
@@ -5,7 +5,7 @@ public class kill : MonoBehaviour
     [SerializeField] Vector2 RespawnPoint;
     void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject.CompareTag(("Player"))){
-        //    Debug.Log("Player Toccato!");
+            Debug.Log("Player Toccato!");
             collision.transform.position = RespawnPoint;
         }
     }
